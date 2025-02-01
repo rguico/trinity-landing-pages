4a76d381579f2f7499a1868b1f9c20c1438b0977
Robert Guico <rguico@gmail.com>
2025-01-31 19:48:41 -0600

    Looks better this way.

--- mission/index.php
+++ mission/index.php
@@ -18,12 +18,19 @@
         <p>Don't worry; you do <em>not</em> need to be Lutheran or a member at Trinity Lutheran Church in order for your kids to attend the school.</p>
         <p><strong>Let us show you the difference in person.</strong> Fill out and submit the form, and we'll send you more information about our faith, activities, and curriculum.</p>
     </main>
-    <section class="left-sidebar"><?php require_once("../ac/form_integration.html"); ?></section>
-    <aside class="right-sidebar">
-        <img src="../img/mission-to-trinity.png" alt="A map from Mission Church to Trinity Lutheran School in Roselle.">
+
+    <?php require_once("../ac/form_integration.html"); ?>
+
+    <pre-footer>
+        <h2>How to Get Here</h2>
+        <img 
+            src="../img/mission-to-trinity.png" 
+            alt="A map from Mission Church to Trinity Lutheran School in Roselle." 
+            style="max-width: 600px;">
         <h2>Hear what Trinity's Girls Varsity Volleyball Team has to say:</h2>
         <?php require_once("../yt/volleyball.html"); ?>
-    </aside>
+    </pre-footer>
+
     <footer class="footer">
         <?php require_once("../includes/footer.php"); ?>
     </footer>
