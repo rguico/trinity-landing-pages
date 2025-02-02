2c1b93e8e20ded397297f570fe15910841e33bac
Robert Guico <rguico@gmail.com>
2025-02-02 10:28:14 -0600

    How to get here.

--- bloomingdale/index.php
+++ bloomingdale/index.php
@@ -13,7 +13,7 @@
     <?php require_once("../includes/header.php"); ?>
     <main class="main-content">
         <h1>Quality Christian Education</h1>
-        <h2>is less than <strong>15</strong> minutes away</h2>
+        <h2>is less than <strong>12</strong> minutes away</h2>
         <p>Trinity Lutheran School is conveniently located 1/2 mile south of downtown Roselle, just off Roselle Road. Trinity is the <em>only</em> Protestant private school in the Roselle-Bloomingdale area.</p>
         <p>You do <em>not</em> need to be Lutheran or a member at Trinity Lutheran Church in order for your kids to attend the school.</p>
         <p><strong>Let us show you the difference in person.</strong> Fill out and submit the form, and we'll send you more information about our faith, activities, and curriculum.</p>
