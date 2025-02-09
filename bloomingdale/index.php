ac02258ac7194cdceed4e187dc975c212c3c4a51
Robert Guico <rguico@gmail.com>
2025-02-09 10:17:36 -0600

    Set up counting.

--- bloomingdale/index.php
+++ bloomingdale/index.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 
 <html>
-<head>
-    <link rel="stylesheet" type="text/css" href="../main.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-</head>
+<?php require_once("../includes/html-header.php"); ?>
 
 <!-- https://matthewjamestaylor.com/holy-grail-layout -->
 <div class="holy-grail-flexbox">
---
+++ includes/html-header.php
@@ -0,0 +1,7 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="../main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<?php require_once("ga.php"); ?>
