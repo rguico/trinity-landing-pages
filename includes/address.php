671dbaae5e6f91ee555a24e1386f54ec8406b288
Robert Guico <rguico@gmail.com>
2025-02-09 22:43:49 -0600

    Text links.

--- includes/address.php
+++ includes/address.php
@@ -1,7 +1,19 @@
 <address>
 405 Rush Street<br/>
 Roselle, Illinois 60172<br/>
-<a href="https://maps.apple.com/?address=405,Rush+Street,Roselle,Illinois&dirflg=d
-"><img src="/img/apple-maps.png" style="width: 50px;"></a>&nbsp;<a href="https://www.google.com/maps/dir/?api=1&destination=405+Rush+Street,+Roselle,+IL
-"><img src="/img/google-maps.png" style="width: 50px;"></a>
+<table style="margin: 0 auto;">
+    <tr>
+        <td><a href="https://maps.apple.com/?address=405,Rush+Street,Roselle,Illinois&dirflg=d
+        ">Apple Maps</a></td>
+        <td><a href="https://maps.apple.com/?address=405,Rush+Street,Roselle,Illinois&dirflg=d
+        "><img src="/img/apple-maps.png" style="width: 50px;"></a></td>
+    </tr>
+    <tr>
+        <td>
+            <a href="https://www.google.com/maps/dir/?api=1&destination=405+Rush+Street,+Roselle,+IL
+    ">Google Maps</a></td>
+        <td><a href="https://www.google.com/maps/dir/?api=1&destination=405+Rush+Street,+Roselle,+IL
+        "><img src="/img/google-maps.png" style="width: 50px;"></a></td>
+    </tr>
+</table>
 </address>
